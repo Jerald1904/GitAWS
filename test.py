7f6e23987b3b4a6b18e571f8562c073b0b8ced9f
Jerald1904 <156393612+Jerald1904@users.noreply.github.com>
2025-03-13 17:35:07 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,34 @@
+Python 3.14.0a5 (tags/v3.14.0a5:3ae9101, Feb 11 2025, 17:36:54) [MSC v.1942 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("Hello, World!")
+... 
+... # Basic Variables
+... a = 10
+... b = 5.5
+... c = "Python Basics"
+... 
+... print(a, b, c)
+... 
+... # Basic List
+... my_list = [1, 2, 3, 4, 5]
+... print(my_list)
+... 
+... # Basic Loop
+... for i in my_list:
+...     print(i)
+... 
+... # Basic Function
+... def add(x, y):
+...     return x + y
+... 
+... print(add(3, 5))
+... 
+... # Basic Conditional
+... if a > b:
+...     print("a is greater than b")
+... else:
+...     print("b is greater or equal to a")
+... 
+... # Basic Dictionary
+... my_dict = {"name": "John", "age": 25}
+... print(my_dict["name"], my_dict["age"])
